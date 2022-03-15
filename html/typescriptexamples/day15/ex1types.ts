8586a4b9c2f7697266c772099258f97f898e095c
nitesh <yadavnitesh060898@gmail.com>
2022-03-15 17:47:38 +0530

    day 17 done

--- html/typescriptexamples/day15/ex1types.ts
+++
@@ -1,7 +0,0 @@
-let username:string='nitesh';
-let age:number=35;
-let isEmployed:boolean=true;
-let contacts:any={phone:987654432,email:"nitesh123@gmail.com"};
-
-document.write(`<p>Username:${username},Age:${age},isEmployed:${isEmployed}</p>`);
-document.write(`<p>Phone:${contacts.phone},Email:${contacts.email}</p>`);
