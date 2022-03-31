13754898a316e24a29d5233646f602e5827bf000
nitesh <yadavnitesh060898@gmail.com>
2022-03-31 17:29:47 +0530

    done

---
+++ node.js/day27/Chat app/index.js
@@ -0,0 +1,26 @@
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+app.get('/', function(req, res){
+  var express=require('express');
+  app.use(express.static(path.join(__dirname)));
+  res.sendFile(path.join(__dirname, '../Chat-app', 'index.html'));
+});
+
+
+io.on('connection', function(socket){ 
+  socket.on('chatMessage', function(from, msg){
+    io.emit('chatMessage', from, msg);
+  });
+  socket.on('notifyUser', function(user){
+    io.emit('notifyUser', user);
+  });
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
