34aab44804e2be5fd094cf1cd166df8698f6185d
nitesh <yadavnitesh060898@gmail.com>
2022-04-12 17:52:42 +0530

    done

---
+++ angular/day36/my-ninth-app/src/app/activity1/activity1.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-activity1',
+  templateUrl: './activity1.component.html',
+  styleUrls: ['./activity1.component.css']
+})
+export class Activity1Component implements OnInit {
+
+  constructor( private builder:FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+  FormDetail=this.builder.group({
+    firstname:[''],
+    lastname:[''],
+    address : this.builder.group({
+      state:[''],
+      city:[''],
+      pin:['']
+    })
+  });
+  
+  handleSubmit(){
+  console.log(this.FormDetail.value);
+  this.FormDetail.reset({});
+    
+  }
+
+}
